1b170fc1fa7775d8dfd4938785179a3547cd5836
Kristin Kraemer <kristinhannahk@gmail.com>
2019-04-16 04:18:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,10 @@
 def oxford_comma(array)
   if array.length == 1 
     return "#{array[0]}"
+  elsif array.length == 2 
+    return "array.join"
   else 
-  last_word = array.pop
-  return "#{array.join(", ")}, and #{last_word}"
+    last_word = array.pop
+    return "#{array.join(", ")}, and #{last_word}"
 end
 end 
